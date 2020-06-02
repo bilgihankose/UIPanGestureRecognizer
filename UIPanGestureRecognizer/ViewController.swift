29ab634c5b440a60b02898aadfcf09ab25ddbc2f
Bilgihan Kose <bilgihankose@gmail.com>
2020-06-02 22:41:21 +0300

    added pan gesture

--- UIPanGestureRecognizer/ViewController.swift
+++ UIPanGestureRecognizer/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let imageView = UIImageView(image: UIImage(named: "swiftlogo"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupViews()
     }
-
-
+    
+    func setupViews() {
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        imageView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture)))
+        imageView.isUserInteractionEnabled = true
+    }
+    
+    
+    @objc func handlePanGesture(gesture: UIPanGestureRecognizer) {
+        //  if gesture.state == .began {
+        //  print("began")
+        if gesture.state == .changed {
+            // print("changed")
+            let translation = gesture.translation(in: view)
+            imageView.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        } else if gesture.state == .ended {
+            //  print("ended")
+            UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.4,  options: .allowAnimatedContent, animations: {self.imageView.transform = .identity})
+        }
+    }
 }
 
